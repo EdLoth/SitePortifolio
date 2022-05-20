fc0f34a128e78d9f02035d5c6b629b1f1e41d330
Eduardo <eduardosram10@gmail.com>
2022-05-20 15:05:52 -0300

    Started Project NextJS

---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import { FaArrowRight } from 'react-icons/fa'
+import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyle'
+import Link from 'next/link'
+
+import Lottie from 'react-lottie';
+import animationData from '../../public/images/animation.json';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
+
+export default function Home() {
+  return (
+    <Container>
+      <Content>
+        <Infos>
+          <Name>Olá, sou Eduardo Ramos</Name>
+          <Function>FullStack Developer</Function>
+          <Intro>Graduado em Sistemas de Informação,
+            tenho experiência em desenvolvimento Web
+            (React, Asp, JS e outros), mobile e criação de conteúdo para o YouTube.
+          </Intro>
+          <Link href="projects">
+            <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+          </Link>
+        </Infos>
+        <Logo>
+          <Lottie
+            options={defaultOptions}
+            style={{ marginLeft: "10vh", marginBottom: "10vh"}}
+            height={400}
+            width={540}
+          />
+        </Logo>
+      </Content>
+    </Container>
+  )
+}
